6f684d8e6210acbcaf78e93cf731bfe8d6e6d7de
Thomas Golbourn <player2dz@gmail.com>
2018-03-05 20:47:42 +0000

    Initial Version
    Contributions at this stage:
    All of the design was split between Harvey & Lucy.
    All of the programming was done by Harvey.
    All of the artwork was done by Lucy.
    No contributions by Tom so far as he joined the group late.

---
+++ UnityProject/Assets/scripts/gridLayout.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gridLayout : MonoBehaviour {
+
+    public GameObject square;
+    public int gridX=10, gridY=20;
+    public GameObject[,] squareArray;
+    public float tileSpacing = 10.1f;
+
+	void Start () {
+        squareArray = new GameObject[gridX,gridY];
+        for (int i = 0; i < gridX; i++)
+        {
+                for (int n = 0; n < gridY; n++)
+                {
+                GameObject gridTemp = Instantiate(square, new Vector3(transform.position.x+i*tileSpacing,transform.position.y+n*tileSpacing), square.transform.rotation) as GameObject;
+                gridTemp.name = "grid:" + i + "," + n;
+                gridTemp.transform.parent = transform;
+                squareArray[i,n] = gridTemp;
+            }
+        }
+    }
+	
+	void Update () {
+		
+	}
+}
---
+++ UnityProject/Assets/scripts/touchManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class touchManager : MonoBehaviour {
+
+    public bool touchDown,isTouching,touchReleased=false;
+    Touch info;
+    Action<Touch> touchUp;
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+    
+        if(Input.touchCount>0)
+        {
+            checkTouchRelease();
+        }
+        else
+        {
+            touchDown = false;
+        }
+
+
+    }
+    public static bool checkTouchRelease()
+    {
+        bool touchReleased = false;
+        for (int i = 0; i < Input.touches.Length; i++)
+        {
+            touchReleased = Input.touches[i].phase == TouchPhase.Ended;
+            print(touchReleased);
+            if (touchReleased)
+                break;
+        }
+        return touchReleased;
+    }
+}
